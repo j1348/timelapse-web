639276af5d1c6575196c907c3c47c23dd3490b77
j1348 <me@jfroffice.me>
2017-01-17 01:23:00 +0100

    Fix ES6 missing string.prototype.startswith

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom';
 import 'medium-draft/lib/index.css';
 import App from './App';
 
+require('string.prototype.startswith');
 require('../css/style.scss');
 
 ReactDOM.render(<App />, document.getElementById('main'));
