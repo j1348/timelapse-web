de689c7ddf6ce6d309debb1533c7a0b237dca5f4
j1348 <me@jfroffice.me>
2017-08-06 20:16:05 +0200

    Move to Webpack 2

---
+++ postcss.config.js
@@ -0,0 +1,8 @@
+/* eslint-disable import/no-commonjs */
+const autoprefixer = require('autoprefixer');
+
+module.exports = {
+    plugins: [
+        autoprefixer({browsers: '> 1%, Last 2 versions, IE 9'}),
+    ],
+};
--- webpack.config.js
+++ webpack.config.js
@@ -8,32 +8,63 @@ const webpack = require('webpack');
 const ENV = process.env.NODE_ENV || 'development';
 const API_URL = process.env.API_URL;
 
+const faviconPlugin = new FaviconsWebpackPlugin({
+   // Your source logo
+   logo: './logo.png',
+   // The prefix for all image files (might be a folder or a name)
+   prefix: 'icons-[hash]/',
+   // Emit all stats of the generated icons
+   emitStats: false,
+   // The name of the json containing all favicon information
+   statsFilename: 'iconstats-[hash].json',
+   // Generate a cache file with control hashes and
+   // don't rebuild the favicons until those hashes change
+   persistentCache: true,
+   // Inject the html into the html-webpack-plugin
+   inject: true,
+   // favicon background color (see https://github.com/haydenbleasel/favicons#usage)
+   background: '#fff',
+   // favicon app title (see https://github.com/haydenbleasel/favicons#usage)
+   title: 'Webpack App',
+
+   // which icons should be generated (see https://github.com/haydenbleasel/favicons#usage)
+   icons: {
+     android: true,
+     appleIcon: true,
+     appleStartup: true,
+     coast: false,
+     favicons: true,
+     firefox: true,
+     opengraph: false,
+     twitter: false,
+     yandex: false,
+     windows: false
+   }
+});
+
 module.exports = {
   devtool: 'source-map', //eval-cheap-module-source-map',
-  entry: [
-    './src/index'
-  ],
+  entry: './src/index',
   output: {
     path: path.join(__dirname, 'dist'),
+    publicPath: './',
     filename: 'bundle.js',
-    publicPath: './'
   },
   module: {
-    loaders: [{
-        test: /\.scss$/,
-        loader: ExtractTextPlugin.extract('style', [
-                  'css',
-                  'sass'
-              ].join('!'))
-      },{
-        test: /\.css$/,
-        loader: ExtractTextPlugin.extract('style', [
-                  'css'
-              ].join('!'))
+    rules: [{
+        test: /\.(scss|css)$/,
+        use: ExtractTextPlugin.extract({
+                fallback: 'style-loader?sourceMap',
+                use: [
+                  'css-loader?sourceMap',
+                  'postcss-loader?sourceMap',
+                  'sass-loader?sourceMap',
+                ],
+            }),
       },{
         test: /\.js$/,
-        loaders: ['babel'],
-        include: path.join(__dirname, 'src')
+        use: ['babel-loader'],
+        include: path.join(__dirname, 'src'),
       }]
   },
   plugins: [
@@ -41,39 +72,7 @@ module.exports = {
         { from: 'dummy.html' },
         { from: '_redirects' }
     ]),
-    new FaviconsWebpackPlugin({
-       // Your source logo
-       logo: './logo.png',
-       // The prefix for all image files (might be a folder or a name)
-       prefix: 'icons-[hash]/',
-       // Emit all stats of the generated icons
-       emitStats: false,
-       // The name of the json containing all favicon information
-       statsFilename: 'iconstats-[hash].json',
-       // Generate a cache file with control hashes and
-       // don't rebuild the favicons until those hashes change
-       persistentCache: true,
-       // Inject the html into the html-webpack-plugin
-       inject: true,
-       // favicon background color (see https://github.com/haydenbleasel/favicons#usage)
-       background: '#fff',
-       // favicon app title (see https://github.com/haydenbleasel/favicons#usage)
-       title: 'Webpack App',
-
-       // which icons should be generated (see https://github.com/haydenbleasel/favicons#usage)
-       icons: {
-         android: true,
-         appleIcon: true,
-         appleStartup: true,
-         coast: false,
-         favicons: true,
-         firefox: true,
-         opengraph: false,
-         twitter: false,
-         yandex: false,
-         windows: false
-       }
-    }),
+    faviconPlugin,
     new webpack.DefinePlugin({
       'process.env': {
         API_URL: JSON.stringify(process.env.API_URL),
@@ -81,7 +80,11 @@ module.exports = {
       }
     }),
     new webpack.ContextReplacementPlugin(/moment[\/\\]locale$/, /fr/),
-    new ExtractTextPlugin('style.css'),
+    new ExtractTextPlugin({
+        filename: 'style.css',
+        allChunks: true,
+        disable: ENV === 'production',
+    }),
     new HtmlWebpackPlugin({
       template: './index.html',
       hash: true,
@@ -90,26 +93,35 @@ module.exports = {
   ].concat(ENV === 'production' ? [
         new webpack.optimize.UglifyJsPlugin({
             minimize: true,
+            sourcemap: false,
+            compress: {
+                warnings: false,
+            },
             comments: /^\**!|^ [0-9]+ $|@preserve|@license/
         })
     ] : []),
+  stats: {
+    colors: true,
+  },
   devServer: {
     port: process.env.PORT || 8000,
     host: '0.0.0.0',
-    colors: true,
     publicPath: '/',
+    disableHostCheck: true,
     contentBase: './',
-    // historyApiFallback: true,
-    proxy: {
-      '/user/login': {
-          target: API_URL
-      },
-      '/user': {
-          target: API_URL
-      },
-      '/todo': {
-          target: API_URL
-      }
-    }
+    historyApiFallback: true,
+    proxy: [{
+        path: '/user/login',
+        logLevel: 'debug',
+        target: API_URL,
+    }, {
+        path: '/user',
+        logLevel: 'debug',
+        target: API_URL,
+    }, {
+        path: '/todo',
+        logLevel: 'debug',
+        target: API_URL,
+    }]
   }
 };
