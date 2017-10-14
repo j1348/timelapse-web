12ae31b904fc567ed2cc7c9be2365629e884cea5
Grzegorz Pasieka <pasieka.grzegorz@gmail.com>
2017-10-14 17:21:04 +0200

    Add react-router-dom. Change child routing and Link to NavLink

--- src/Header.js
+++ src/Header.js
@@ -1,38 +1,44 @@
 import React from 'react';
-import { Link } from 'react-router';
+import { Route } from 'react-router';
+import { NavLink } from 'react-router-dom'
+import Login from './Login';
+import SignUp from './SignUp';
 import Alert from 'react-s-alert';
 
 export default function(props) {
     return (
-        <div className="header">
-            <Alert stack={{ limit: 1 }} />
-            <div className="header-col">
-                <h2>Smashing Time</h2>
-            </div>
-            <div className="header-col">
-                <nav className="nav">
-                    <ul>
-                        <li>
-                            <Link
-                                to="/login"
-                                name="login"
-                                activeClassName="active"
-                            >
-                                Login
-                            </Link>
-                        </li>
-                        <li>
-                            <Link
-                                to="/signup"
-                                name="signup"
-                                activeClassName="active"
-                            >
-                                Sign-Up
-                            </Link>
-                        </li>
-                    </ul>
-                </nav>
-                {props.children}
+        <div>
+            <div className="header">
+                <Alert stack={{ limit: 1 }} />
+                <div className="header-col">
+                    <h2>Smashing Time</h2>
+                </div>
+                <div className="header-col">
+                    <nav className="nav">
+                        <ul>
+                            <li>
+                                <NavLink
+                                    to="/login"
+                                    name="login"
+                                    activeClassName="active"
+                                >
+                                    Login
+                                </NavLink>
+                            </li>
+                            <li>
+                                <NavLink
+                                    to="/signup"
+                                    name="signup"
+                                    activeClassName="active"
+                                >
+                                    Sign-Up
+                                </NavLink>
+                            </li>
+                        </ul>
+                    </nav>
+                    <Route path="/login" component={Login} />
+                    <Route path="/signup" component={SignUp} />
+                </div>
             </div>
         </div>
     );
--- src/index.js
+++ src/index.js
@@ -1,27 +1,24 @@
 import React from 'react';
 import { render } from 'react-dom';
-import { Router, Route, hashHistory } from 'react-router';
+import { Route } from 'react-router';
+import { HashRouter } from 'react-router-dom'
 import 'string.prototype.startswith';
 import 'medium-draft/lib/index.css';
 import 'react-s-alert/dist/s-alert-default.css';
 import Raven from 'raven-js';
 import Header from './Header';
-import Login from './Login';
-import SignUp from './SignUp';
 import App from './App';
 import '../css/style.scss';
 
 const SENTRY = 'https://9b7d1cedf93249389f90839b13dbf4c5@sentry.io/129263';
 
 Raven.config(process.env.NODE_ENV !== 'production' ? '' : SENTRY).install();
-
 render(
-    <Router history={hashHistory}>
-        <Route path="/" component={Header}>
-            <Route path="/login" component={Login} />
-            <Route path="/signup" component={SignUp} />
-        </Route>
-        <Route path="/token/:token" component={App} />
-    </Router>,
+    <HashRouter>
+        <div>
+            <Route path="/" component={Header}/>
+            <Route path="/token/:token" component={App} />
+        </div>
+    </HashRouter>,
     document.getElementById('main')
 );
